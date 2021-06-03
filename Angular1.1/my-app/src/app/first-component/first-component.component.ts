05e51022afee5b15e8bb82421734798e1d219744
Santo <santicardonas777@gmail.com>
2021-06-03 14:55:10 -0500

    Se ajusto los cambios en fist-component

--- Angular1.1/my-app/src/app/first-component/first-component.component.ts
+++ Angular1.1/my-app/src/app/first-component/first-component.component.ts
@@ -1,17 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { Component} from '@angular/core';
 
 @Component({
   selector: 'app-first-component',
   templateUrl: './first-component.component.html',
   styleUrls: ['./first-component.component.css']
 })
-export class FirstComponentComponent implements OnInit {
+export class FirstComponentComponent{
 
-  constructor() { 
+  img = 'https://thecatapi.com/api/images/get?format=src&type=gif?results_per_page=';
 
-  }
-
-  ngOnInit(): void {
-  }
 
 }
